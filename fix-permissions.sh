bc90f6f47ceef0374d5969ad065ae240cd85ff79
Sebastian Mäki <sebastian@tico.fi>
2013-08-02 22:05:06 +0300

    More parmission changes

--- fix-permissions.sh
+++ fix-permissions.sh
@@ -8,7 +8,7 @@ if [ -z "$1" ]; then
 	exit 1;
 fi;
 
-if which chcon; then
+if which chcon &> /dev/null; then
 	SELINUX=true;
 else
 	SELINUX=false;
@@ -16,6 +16,9 @@ fi;
 
 if [ $DEVEL ]; then
 	find "$1/protected" -type d -exec chmod ug+rwx {} \;
+	if $SELINUX; then
+		chcon -R -t httpd_sys_rw_content_t "$1/protected"
+	fi;
 fi;
 
 chmod ug+rwx "$1/assets"
@@ -27,4 +30,12 @@ find "$1/protected/runtime" -type f -exec chmod ug+rw {} \;
 find "$1/protected/runtime" -type f -exec chmod a-x {} \;
 chmod go-w "$1/protected/runtime/index.html"
 chmod -R a-w "$1/yii"
+
+if $SELINUX; then
+	chcon -R -t httpd_sys_rw_content_t "$1/assets"
+	chcon -t httpd_sys_content_t "$1/assets/index.html"
+	chcon -R -t httpd_sys_rw_content_t "$1/protected/runtime"
+	chcon -t httpd_sys_content_t "$1/protected/runtime/index.html"
+fi;
+
 
